60b56b7b69ff24c79b4efd95efb4043e50ac3add
powerfullbot <powerfull521@gmail.com>
2016-02-01 06:49:13 -0800

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (ztesbot)",-- Put everything you like :)
+		"^[!/]([Zz][Tt][Ee][Ss][Bb][Oo][Tt])"-- Put everything you like :)
     }, 
   run = run
 }
